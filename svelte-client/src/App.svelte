e6603aea25a5f28561c6afc52524c3dca650824a
tamari gray <tamarigray97@gmail.com>
2024-11-16 17:20:46 +1300

    show vote percentage overlay frontend

--- svelte-client/src/App.svelte
+++ svelte-client/src/App.svelte
@@ -1,11 +1,13 @@
-<script>
-  import { Router, Route, Link } from 'svelte-routing';
+ <script>
+  import { Router, Route } from 'svelte-routing';
   import Leaderboard from './lib/components/leaderboard.svelte';
+  import winPercentage from './lib/components/winPercentage.svelte'
 
 </script>
 
 <main>
   <Router>
     <Route path="/leaderboard" component={Leaderboard} />
+    <Route path="/win-percentage" component={winPercentage} />
   </Router>
 </main>
---
+++ svelte-client/src/lib/components/winPercentage.svelte
@@ -0,0 +1,50 @@
+<script>
+ let winPercent = 60; // Set the win percentage here (can be dynamic)
+ let name = 'Jenna'; // Static variable for the name
+ let losePercent = 100 - winPercent; // Calculate the lose percentage
+</script>
+
+<style>
+ .win-percentage {
+   width: 1440px;
+   height: 270px; /* Set a fixed height for the component */
+   display: flex;
+   font-family: 'Burbank Big Condensed', sans-serif;
+ }
+
+ .win {
+   background-color: red;
+   width: var(--win-width);
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   color: white;
+   font-weight: bold;
+   text-align: center;
+ }
+
+ .lose {
+   background-color: blue;
+   width: calc(100% - var(--win-width));
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   color: white;
+   font-weight: bold;
+   text-align: center;
+ }
+
+ .label {
+   font-size: 36px;
+   font-weight: bold;
+ }
+</style>
+
+<div class="win-percentage" style="--win-width: {winPercent}%">
+ <div class="win">
+   <span class="label">{winPercent}% voted {name} wins</span>
+ </div>
+ <div class="lose">
+   <span class="label">{losePercent}% voted {name} loses</span>
+ </div>
+</div>
