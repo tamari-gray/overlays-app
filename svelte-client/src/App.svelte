4ed9c4698162f9128b3ffac16bf9c1449cb0947f
tamari gray <tamarigray97@gmail.com>
2024-12-15 18:27:24 +1300

    rules animation!

--- svelte-client/src/App.svelte
+++ svelte-client/src/App.svelte
@@ -3,6 +3,7 @@
   import Leaderboard from './lib/components/leaderboard.svelte';
   import winPercentage from './lib/components/winPercentage.svelte';
   import StreamInfo from './lib/components/streamInfo.svelte';
+  import Rules from './lib/components/rules.svelte';
 
 </script>
 
@@ -11,6 +12,7 @@
     <Route path="/leaderboard" component={Leaderboard} />
     <Route path="/win-percentage" component={winPercentage} />
     <Route path="/stream-info" component={StreamInfo} />
+    <Route path="/rules" component={Rules} />
 
   </Router>
 </main>
